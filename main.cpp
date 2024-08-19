47a2d6fc835e311cb2af77fec1d084540e72270b
Wade Johnson <wade@wadeintheweb.ca>
2024-08-18 18:43:23 -0600

    Added a new line for the NewBranch

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 int main() {
 
-	std::cout << "Hello Savvy People, this is my repo";
+	std::cout << "Hello Savvy People, this is my repo\n";
+	std::cout << "and now I have added a branch\n";
 	return 0;
 }
